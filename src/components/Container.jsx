af032a3d10d9d7b80341941715a502a938f1df9d
Erick Henriquez <henriquez93550@yahoo.com>
2018-12-08 00:24:30 -0600

    added mad libs forms

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,20 +2,58 @@ import React, { Component } from 'react';
 import './Container.css';
 
 class Container extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            noun: '',
+            adjective: '',
+            adverb: ''
+        }
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        // const data = this.data
+        // console.log("This was sent to backend", data);
+    }
+
+    handleInputChange = (event) => {
+        event.preventDefault()
+        // console.log(event);
+        // console.log(event.target.name);
+        // console.log(event.target.value);
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+
+    }
     render() {
+        const {noun} = this.state
+        const {adjective} = this.state
+        const {adverb} = this.state
+
         return (
             <div className="grid">
-          
                 <div className="madlibwrap">
-                <div className="css-typing">
-                    <p>{this.props.madlib}</p>
-                    </div>
-                        <button type="submit"> Results</button>
-
+                    <p>Driving a car can be fun if you follow this {adjective} advice:
+When approaching a {noun} on the right, always blow your {noun} 
+  Before making a {adjective} turn, always stick your {noun} out of the window.
+Every 2000 miles, have your {noun} inspected and your {noun} checked.
+When approaching a school, watch out for {adjective} [plural noun]
+Above all, drive {adverb} The {noun} you save may be your own!</p>
+                    <form onSubmit={this.handleSubmit}>
+                        <p><input type='text' placeholder='noun' name='noun' onChange={this.handleInputChange} /></p>
+                        <p><input type='text' placeholder='adjective' name='adjective' onChange={this.handleInputChange} /></p>
+                        <p><input type='text' placeholder='adverb' name='adverb' onChange={this.handleInputChange} /></p>
                     
-
+                    </form>
+                    {/* <button type="submit"> Results</button> */}
                 </div>
+
+
+
             </div>
+
         );
     }
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+
 import Navbar from '../components/Navbar.jsx';
 import Footer from '../components/Footer.jsx';
 
@@ -11,14 +12,10 @@ class Contact extends Component {
         <div>
           <Navbar />
          
-      
-          < Container madlib="Driving a car can be fun if you follow this [adjective] advice:
-When approaching a [noun] on the right, always blow your [noun]
-Before making a [adjective]turn, always stick your [noun]out of the window.
-Every 2000 miles, have your [noun]inspected and your [noun]checked.
-When approaching a school, watch out for [adjective] [plural noun]
-Above all, drive [adverb] The [noun] you save may be your own!
-" />
+          
+          < Container />
+         
+
         
           <Footer />
         </div>
