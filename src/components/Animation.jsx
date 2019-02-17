9ae67db107b7a2058b65453c63a573e1e9f716f3
Erick Henriquez <henriquez93550@yahoo.com>
2019-02-17 14:05:28 -0600

    minor changes

--- src/components/Animation.jsx
+++ src/components/Animation.jsx
@@ -1,29 +1,28 @@
-import React, { Component } from 'react';
-import './Animation.css';
-
+import React, { Component } from "react";
+import "./Animation.css";
 
 class Animation extends Component {
-    render() {
-        return (
-            <div className="animation">
-            <h3>Mad Libs!</h3>
-            <div class="buttonHolder">
-            <button className="buttonA"><a href="/form">Begin</a></button>
-           
-            </div>
-            <div className="sky" ></div >
-            <img className="cloud" src="/images/cloud.png" alt="" />
-            <img className="cloud2" src="/images/cloud.png" alt="" />
-                <div className="grass"></div>
-                <div className="road">
-                    <div className="lines"></div>
-                    <img className="mario" src="/images/mario.png" alt="" />
-                    <img className="luigi" src="/images/luigi.png" alt="" />
-                </div>
-                </div>
-
-        );
-    }
+  render() {
+    return (
+      <div className="animation">
+        <h3>Mad Libs!</h3>
+        <div class="buttonHolder">
+          <button className="buttonA">
+            <a href="/">Begin</a>
+          </button>
+        </div>
+        <div className="sky" />
+        <img className="cloud" src="/images/cloud.png" alt="" />
+        <img className="cloud2" src="/images/cloud.png" alt="" />
+        <div className="grass" />
+        <div className="road">
+          <div className="lines" />
+          <img className="mario" src="/images/mario.png" alt="" />
+          <img className="luigi" src="/images/luigi.png" alt="" />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Animation
+export default Animation;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,57 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 class Navbar extends Component {
   render() {
     return (
-        <nav className="navbar navbar-expand-lg">
-        <div className="container"> {/*container to center navbar contents */}
-        <Link className="navbar-brand" to="/animation">Mad Libs!</Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-         
-        <div className="collapse navbar-collapse" id="navbarText">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item active">
-              <Link className="nav-link" to="/">Mad Lib 1 <span className="sr-only">(current)</span></Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">Mad Lib 2</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">Mad Lib 3</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/results">Results</Link>
-            </li>
-            
-            {/* <li className="nav-item">
+      <nav className="navbar navbar-expand-lg">
+        <div className="container">
+          {" "}
+          {/*container to center navbar contents */}
+          <Link className="navbar-brand" to="/animation">
+            Mad Libs!
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarText"
+            aria-controls="navbarText"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarText">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <Link className="nav-link" to="/">
+                  Mad Lib 1 <span className="sr-only">(current)</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">
+                  Mad Lib 2
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/contact">
+                  Mad Lib 3
+                </Link>
+              </li>
+
+              {/* <li className="nav-item">
               <Link className="nav-link" to="/form">Form</Link>
             </li> */}
-            {/* <li className="nav-item">
+              {/* <li className="nav-item">
               <Link className="nav-link" to="/animation">Animation</Link>
             </li> */}
-          </ul>
-        </div>
-        </div>  {/*end of container */}
+            </ul>
+          </div>
+        </div>{" "}
+        {/*end of container */}
       </nav>
-      
     );
   }
 }
