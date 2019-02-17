f739bd80f68f75cb88a47780c697ca6bc92f94ec
Erick Henriquez <henriquez93550@yahoo.com>
2019-02-17 14:19:22 -0600

    Updates

---
+++ precache-manifest.da4b5a51ed5d18e18a3ea940f21ecf6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5849dea0b78eb4508abc",
+    "url": "/react-story-app/static/css/main.3796a000.chunk.css"
+  },
+  {
+    "revision": "5849dea0b78eb4508abc",
+    "url": "/react-story-app/static/js/main.5849dea0.chunk.js"
+  },
+  {
+    "revision": "97dd6a76663588ce1eec",
+    "url": "/react-story-app/static/js/1.97dd6a76.chunk.js"
+  },
+  {
+    "revision": "63c050b81279cf13c903",
+    "url": "/react-story-app/static/js/runtime~main.63c050b8.js"
+  },
+  {
+    "revision": "d1f121318b8b8599394447448ace389a",
+    "url": "/react-story-app/index.html"
+  }
+];
